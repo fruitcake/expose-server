5fad6f9cb584fb696ebf2c05e48aa8a6f4f5da4d
Barry vd. Heuvel <barry@fruitcake.nl>
2025-07-16 17:09:54 +0200

    set db path

--- config/expose-server.php
+++ config/expose-server.php
@@ -21,11 +21,11 @@ return [
     | if you enable authentication token validation.
     |
     */
-    'database' => implode(DIRECTORY_SEPARATOR, [
+    'database' => env('DB_DATABASE', implode(DIRECTORY_SEPARATOR, [
         $_SERVER['HOME'] ?? __DIR__,
         '.expose',
         'expose.db',
-    ]),
+    ])),
 
     /*
     |--------------------------------------------------------------------------
